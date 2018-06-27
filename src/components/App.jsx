30e95f2fa11f68efb9adcdf1ef63cb546e6680dc
Guest User <Guest@Epicodus-7E.local>
2018-06-27 08:17:24 -0700

    create a new file called new ticketform and import all the switches and routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import TicketList from './TicketList'
 import Header from './Header'
+import { Switch, Route } from 'react-router-dom'
+import NewTicketForm from './NewTicketForm'
 
 function App(){
   let appStyle = {
@@ -10,7 +12,10 @@ function App(){
   return (
     <div style={appStyle}>
       <Header/>
-      <TicketList />
+      <Switch>
+        <Route exact path='/' component={TicketList}/>
+        <Route path='/newticket' component={NewTicketForm} />
+      </Switch>
     </div>
   );
 }
